c3c0dd629c4610f9e40e96021506c25ad3906caa
kimbyeonggeun <qudrms2157@gmail.com>
2023-02-13 13:39:40 +0900

    add files

--- src/ApiExplorersWithDatagokr_self.java
+++ src/ApiExplorersWithDatagokr_self.java
@@ -7,36 +7,31 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 
-public class ApiExplorersWithDatagokr {
+public class ApiExplorersWithDatagokr_self {
     public static void main(String[] args) throws UnsupportedEncodingException {
         /*
-         * 외교부 국가/지역별 최신 안전소식(코로나 관련) - 중국
-         * https://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/
-         * getCountrySafetyNewsListNew
+         * 지진해일 관련 긴급대피장소에 대한 시도명, 시군구명, 대피지구명, 위/경도 값을 제공하는 지진해일 긴급대피 장소 서비스
+         * https://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List
          * ?serviceKey=UEohFgDSeIH%2B3Id96m3wr%
          * 2FZlkE4dgaJMCfYi2OLGzxIShY6I6vcU6zicFQIRKU%2F5LE7kB0XrMwx1x6RgXL0RAA%3D%3D
-         * &returnType=JSON
-         * &numOfRows=10
          * &pageNo=1
-         * &cond[country_nm::EQ]=중국
+         * &numOfRows=10
+         * &type=xml
          */
 
         StringBuilder urlBuilder = new StringBuilder(
-                "http://apis.data.go.kr/1262000/CountryCovid19SafetyServiceNew/getCountrySafetyNewsListNew"); /* URL */
+                "https://apis.data.go.kr/1741000/TsunamiShelter3/getTsunamiShelter1List"); /* URL */
         // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
         // Service Key
         urlBuilder.append("?" + URLEncoder.encode("ServiceKey", "UTF-8") + "=").append(
                 "UEohFgDSeIH%2B3Id96m3wr%2FZlkE4dgaJMCfYi2OLGzxIShY6I6vcU6zicFQIRKU%2F5LE7kB0XrMwx1x6RgXL0RAA%3D%3D");
-        // XML 또는 JSON
-        urlBuilder.append("&" + URLEncoder.encode("returnType", "UTF-8") + "="
-                + URLEncoder.encode("JSON", "UTF-8"));
+        // 페이지 번호
+        urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "="
+                + URLEncoder.encode("1", "UTF-8"));
         // 한 페이지 결과 수
         urlBuilder.append("&" + URLEncoder.encode("numOfRows", "UTF-8") + "=" + URLEncoder.encode("10", "UTF-8"));
-        // 페이지 번호
-        urlBuilder.append("&" + URLEncoder.encode("pageNo", "UTF-8") + "=" + URLEncoder.encode("1", "UTF-8"));
-        // ISO 2자리코드
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]", "UTF-8") + "="
-                + URLEncoder.encode("중국", "UTF-8"));
+        // XML 또는 JSON
+        urlBuilder.append("&" + URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("xml", "UTF-8"));
 
         URL url;
         HttpURLConnection conn = null;
@@ -48,7 +43,7 @@ public class ApiExplorersWithDatagokr {
             System.out.println("Response code : " + conn.getResponseCode());
 
             BufferedReader reader = null;
-            if (conn.getResponseCode() >= 200 && conn.getResponseCode() < 300) { // 정상
+            if (conn.getResponseCode() >= 200 && conn.getResponseCode() < 300) { // 정상(보통 코드가 200 ~ 300사이)
                 reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
             } else { // 비정상
                 reader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
